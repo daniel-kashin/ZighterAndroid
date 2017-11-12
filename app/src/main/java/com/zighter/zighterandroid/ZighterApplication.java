eb1fbc2718b4a87618b0d79b3d7103437be75cc5
danil-kashin <danil-kashin@yandex-team.ru>
2017-11-13 01:34:06 +0300

    Add findbugs, pmd, crashlitycs, leakcanary

---
+++ app/src/main/java/com/zighter/zighterandroid/ZighterApplication.java
@@ -0,0 +1,17 @@
+package com.zighter.zighterandroid;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class ZighterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) return;
+        LeakCanary.install(this);
+    }
+
+}
